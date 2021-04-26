87a8605f8eb658276e1159f72c2961c550425344
henriquemartins <henrm@riseup.net>
2021-04-26 15:26:18 +0200

    i dont know

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="page universe">
+      <div className="content-container galaxy">
+        <div className="lake">
+          <div className="lake__three plane">
+            <div className="box lake__three__box box-1"></div>
+            <div className="box lake__three__box box-2"></div>
+            <div className="box lake__three__box box-3"></div>
+            <div className="box lake__three__box box-4"></div>
+            <div className="box lake__three__box box-5"></div>
+            <div className="box lake__three__box box-6"></div>
+            <div className="box lake__three__box box-7"></div>
+            <div className="box lake__three__box box-8"></div>
+          </div>
+          <div className="lake__two plane">
+            <div className="box lake__two__box box-9"></div>
+            <div className="box lake__two__box box-10"></div>
+            <div className="box lake__two__box box-11"></div>
+            <div className="box lake__two__box box-12"></div>
+          </div>
+          <div className="lake__one plane">
+            <div className="box lake__one__box box-13"></div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
