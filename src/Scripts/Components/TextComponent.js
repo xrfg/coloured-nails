9c1cc7e4e887c901fa6691da27e7e0bf0a41a7f6
henriquemartins <henrm@riseup.net>
2021-05-03 23:09:26 +0200

    adding btn to text comp

--- src/Scripts/Components/TextComponent.js
+++ src/Scripts/Components/TextComponent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { data } from "../../data/data";
 import "../../Sass/Reset.scss";
 import "../../Sass/TextComponent.scss";
 
@@ -13,15 +12,19 @@ export default function TextComponent(props) {
         if (e.target.className === "overlay") props.history.goBack();
       }}
     >
-      <section className="background2-1 text-component">
-        <div>
-          <main className="content-container">
-            <div className="blob">
-              <p className="blob__text">{text}</p>
-              <p className="blob__date">{textDate}</p>
-            </div>
-          </main>
-        </div>
+      <section className="background2-1 text-component-card">
+        <main className="content-container text-component-card__content">
+          <div className="blob">
+            <p className="blob__text">{text}</p>
+            <p className="blob__date">{textDate}</p>
+          </div>
+          <div
+            className="text-component-card__content__closing-btn"
+            onClick={() => props.history.goBack()}
+          >
+            <span>(X)</span>
+          </div>
+        </main>
       </section>
     </div>
   );
